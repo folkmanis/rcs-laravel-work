e144a00d4e295b85369fb4d0f415a8db521da41d
folkmanis <folkmanis@gmail.com>
2023-10-17 10:33:56 +0300

    uses one date in calculations

--- resources/js/Pages/Messages/CreationTime.tsx
+++ resources/js/Pages/Messages/CreationTime.tsx
@@ -11,13 +11,14 @@ function coerceToNow(date: string, now = new Date()): Dayjs {
 }
 
 export function CreationTime({ createdAt, updatedAt }: CreationTimeProps) {
-    let created = coerceToNow(createdAt);
-    let updated = coerceToNow(updatedAt);
+    const now = new Date();
+    let created = coerceToNow(createdAt, now);
+    let updated = coerceToNow(updatedAt, now);
     return (
         <span className="ml-2 text-xs italic">
             {created.isSame(updatedAt)
                 ? created.fromNow()
-                : "labots " + updated.fromNow()}
+                : "labots " + updated.from(now)}
         </span>
     );
 }
