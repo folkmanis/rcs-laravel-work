311b9c08a4214698535d8961f2ccac58211357d1
folkmanis <folkmanis@gmail.com>
2023-10-19 17:12:14 +0300

    photo delete confirmation

--- resources/js/Components/PhotosPartials/Properties.tsx
+++ resources/js/Components/PhotosPartials/Properties.tsx
@@ -1,6 +1,9 @@
 import { Photo } from "@/types";
 import { Caption } from "./Caption";
 import { IconButton } from "../IconButton";
+import DeleteConfirmationDialog from "../DeleteConfirmationDialog";
+import { useState } from "react";
+import { router } from "@inertiajs/react";
 
 interface PropertiesProps {
     photo: Photo;
@@ -13,29 +16,39 @@ export function Properties({
     className = "",
     editable,
 }: PropertiesProps) {
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+    const onDelete = () => {
+        setDeleteConfirmation(false);
+        router.delete(route("photos.destroy", photo.id));
+    };
     return (
-        <div
-            className={
-                "mt-2 grow shrink overflow-hidden flex flex-col text-sm " +
-                className
-            }
-        >
-            <h3 className="font-bold overflow-hidden text-ellipsis">
-                {photo.name}
-            </h3>
-            <Caption photo={photo} editable={editable} />
-            {editable ? (
-                <div className="mt-auto">
-                    <IconButton
-                        href={route("photos.destroy", photo.id)}
-                        method="delete"
-                        as="button"
-                        mini
-                    >
-                        delete
-                    </IconButton>
-                </div>
-            ) : null}
-        </div>
+        <>
+            <div
+                className={
+                    "mt-2 grow shrink overflow-hidden flex flex-col text-sm " +
+                    className
+                }
+            >
+                <h3 className="font-bold overflow-hidden text-ellipsis">
+                    {photo.name}
+                </h3>
+                <Caption photo={photo} editable={editable} />
+                {editable && (
+                    <div className="mt-auto">
+                        <IconButton
+                            mini
+                            onClick={() => setDeleteConfirmation(true)}
+                        >
+                            delete
+                        </IconButton>
+                    </div>
+                )}
+            </div>
+            <DeleteConfirmationDialog
+                onCancel={() => setDeleteConfirmation(false)}
+                onConfirm={onDelete}
+                show={deleteConfirmation}
+            />
+        </>
     );
 }
