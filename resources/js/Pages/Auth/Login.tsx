66d0fb700174a1dceca806a45d685a82b6ecc4fb
folkmanis <folkmanis@gmail.com>
2023-10-19 13:32:08 +0300

    removed status variable

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -3,17 +3,15 @@ import { GuestLayout } from "@/Layouts/GuestLayout";
 import { Head } from "@inertiajs/react";
 
 export default function Login({
-    status,
     canResetPassword,
 }: {
-    status?: string;
     canResetPassword: boolean;
 }) {
     return (
         <GuestLayout>
             <Head title="Pieslēgties" />
 
-            <LoginForm canResetPassword={canResetPassword} status={status} />
+            <LoginForm canResetPassword={canResetPassword} />
         </GuestLayout>
     );
 }
